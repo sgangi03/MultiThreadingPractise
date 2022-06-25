1cb5130ce73d03bdf3b5cdc292166f50d55322ae
Shiva <shiva.yadav18@gmail.com>
2022-06-25 16:18:25 -0400

    "hi"

--- MultiThreading/src/CyclicBarrier/main.java
+++ MultiThreading/src/CyclicBarrier/main.java
@@ -7,7 +7,7 @@ public class main {
 
 	public static void main(String[] args) {
 	
-		Runnable r=()->System.out.println("hi");
+		Runnable r=()->System.out.println ("hi");
 		CyclicBarrier cd=new CyclicBarrier(2,r);
 		
 		Thread1 t1=new Thread1(cd);
