9511879f487d762f4fa9593e1f20fd68b9577858
kswat <kswat@DESKTOP-F0NCFGU.myfiosgateway.com>
2022-06-26 19:09:31 -0400

    t

--- MultiThreading/src/ExecutorService/main.java
+++ MultiThreading/src/ExecutorService/main.java
@@ -3,21 +3,17 @@ package ExecutorService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class main {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 	
-		List<Future> futlist=new ArrayList<>();
+		List<Future<String>> futlist=new ArrayList<>();
 		ExecutorService ex=Executors.newFixedThreadPool(10);
 		for(int i=0;i<100;i++) {
 		Thread2 t2=new Thread2();
@@ -34,12 +30,13 @@ public class main {
 		
 		ex.awaitTermination(1, TimeUnit.MILLISECONDS);
 		
-		for(Future f:futlist) {
+		for(Future<String> f:futlist) {
 			
 			System.out.println("IsCancelled:"+ f.isCancelled());
 			System.out.println("Is Completed:"+ f.isDone());
-			
-			//System.out.println("get:"+ f.get());
+			f.cancel(true);
+			System.out.println("IsCancelled:"+ f.isCancelled());
+		//	System.out.println("get:"+ f.get());
 			System.out.println("Thread completed");
 			System.out.println("Is Completed:"+ f.isDone());
 		}
@@ -69,11 +66,11 @@ public class main {
 }
  
  
- class Thread2 implements Callable{
+ class Thread2 implements Callable<String>{
 
 	@Override
-	public Object call() throws Exception {
-	Thread.sleep(1000);
+	public String call() throws Exception {
+	Thread.sleep(10000);
 		return "Shiva";
 	}
 
